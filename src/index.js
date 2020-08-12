d7f8a8fd6f32cc61d39949e15811cf1edf5b3ba5
Davy Engone <4896002+davyengone@users.noreply.github.com>
2020-08-12 09:23:02 +0200

    added a todo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
+// Query the elements
+const btnAdd = document.getElementById("btnAdd");
+const todoInput = document.querySelector("input");
+const todoList = document.querySelector("ul");
 
+const todosArray = [];
+
+load();
+function load() {
+  let todos = localStorage.getItem("todos");
+  if (todos) {
+    todos = todos.split(",");
+    todos.forEach(function (todo) {
+      addElement(todosArray, todo, todoList);
+    });
+  }
+}
+
+// Add event listener
+btnAdd.addEventListener("click", addTodo);
+
+// Add event handler
+function addTodo(event) {
+  /**
+   * Get a ref of the input's text
+   * Create a <li> element
+   * Add that text into a <li> element
+   * Append that <li> element to te <ul> element
+   * **/
+
+  // 1.
+  const todo = todoInput.value; // Learn JavaScript
+
+  //   if (todo !== "") {
+  if (todo) {
+    addElement(todosArray, todo, todoList);
+
+    // 5. Empty the input value
+    todoInput.value = "";
+
+    // 6. Save to localstorage
+    localStorage.setItem("todos", todosArray);
+  }
+}
+
+function addElement(todos, todo, todoList) {
+  todos.push(todo);
+  const li = document.createElement("li"); // <li></li>
+  li.innerText = todo; // <li>Learn JavaScript</li>
+  todoList.appendChild(li);
+}
+
+// function deleteTodo() {}
+// function toggle() {}
---
+++ tasks.md
@@ -0,0 +1,20 @@
+### 10 things you can do with HTML
+
+#### Add button
+
+When clicking on the button 'Add', add the text in the input into the list of todos then store that information in the DOM
+
+#### Done button
+
+When you click on the button 'Done', add a 'text-decoration' to the todo. If you click again the 'Done' button, remove the 'text-decoration'
+
+#### Delete button
+
+When you click on the button 'Delete', remove the todo from the list of todos then remove from the DOM the actual element.
+
+#### Filters
+
+Add 2 filters:
+1 - See all tasks: It will show all the tasks
+2 - See only the tasks done: It will show only the tasks done
+3 - See tasks left: It will show only the tasks left to complete
